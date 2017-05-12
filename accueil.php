cdbd2d4ab64510f216ef7ac16003753d6c028b65
COTTON CORENTIN <corentin.cotton@free.fr>
2017-05-12 16:20:45 +0200

    first commit

---
+++ accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>document</title>
+    <link rel="stylesheet" href="accueil.css">
+    <link rel="stylesheet" href="bootstrap.css">
+  </head>
+  <body>
+    <header>
+      <div class="connexion">
+        <ul>
+          <li><a href="#">se connecter</a></li>
+          <li><a href="#">s'inscrire</a></li>
+        </ul>
+      </div>
+    </header>
+  </body>
+</html>
